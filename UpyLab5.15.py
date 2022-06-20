cbd05ddbf8163ac54176107fab8505fbd04d8342
cedson22 <96386125+cedson22@users.noreply.github.com>
2022-06-20 12:10:07 +0100

    Add files via upload
    Programme finalisé le 20 juin 2022

--- UpyLab5.15.py
+++ UpyLab5.15.py
@@ -1,3 +1,4 @@
+# noinspection LanguageDetectionInspection
 """
 5.4.11. Exercice UpyLaB 5.15 - Parcours rouge
 (D’après une idée de Jacky Trinh - 11/02/2018)
@@ -32,3 +33,41 @@ représente le mot « lire » ou « lier ». Afin d’éviter cette confusion, d
 moins à une distance de 3. Il n’y aura ainsi qu’un seul mot dans liste_mots répondant au problème.
 Vous pouvez supposer que Joao soit arrive à écrire des mots sans fautes, soit fait au plus une erreur.
 """
+def distance_mots(mot1,mot2):
+    '''Cette fonction calcule le nombre minimum de fois où il faut modifier une
+    lettre de mot_1 pour obtenir mot_2, sans changer leur ordre (distance de Hamming)'''
+    cont = 0
+    for i,j in zip(mot2,range(len(mot1))):
+        if i != mot1[j] :
+            cont +=1
+        else:
+            continue
+    return cont
+
+print(distance_mots('merci','mrcia'))
+
+def correcteur(mot, liste_mots):
+    """ Cette fonction est un utilise un correcteur orthographique"""
+    v =[]
+    v1 = []
+    for j in liste_mots :
+        if len(mot) == len(j):
+            v.append(distance_mots(mot,j))
+            v1.append(j)
+        else:
+            continue
+    c = min(v)
+    for i,a in enumerate (v):
+        if c == v[i]:
+            return v1[i]
+            break
+
+
+
+#print (correcteur("chat", ["chien", "chat", "train", "voiture", "bonjour", "merci"]))
+#print(correcteur("bonvour", ["chien", "chat", "train", "voiture", "bonjour", "merci"]))
+print(correcteur("ancomada", ["chien", "chat", "train", "voiture", "bonjour", "merci",'remettre','anacondi']))
+
+
+
+
